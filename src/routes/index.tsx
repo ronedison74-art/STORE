b1ba114ad690ba43348e9c4df4816cc59b2fca29
ronedison74-art <ronedison74@gmail.com>
2026-09-05 16:31:00 +0800

    reset

--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
-import { getCurrentAccount } from '../server/auth.functions'
-
-export const Route = createFileRoute('/')({
-  beforeLoad: async () => {
-    const account = await getCurrentAccount()
-    throw redirect({ to: account ? '/admin' : '/login' })
-  },
-})
